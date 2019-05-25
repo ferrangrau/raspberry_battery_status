755090710e4881ff6544df66957e467b128ef944
Ferran Grau Horta <ferran.grau@scmspain.com>
2019-05-25 14:16:39 +0200

    change time and set last date

--- run.py
+++ run.py
@@ -16,6 +16,16 @@ divider = 5.7 # see https://www.enigma14.eu/wiki/AD_Converter_PCF8591_for_Raspbe
 
 
 if __name__ == '__main__':
+    date: str = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    charge: str = ""
+
+    """
+    Batería al 100% (totalmente cargada) = 12,7 V (voltios)
+    Batería al 75% = 12,5 V
+    Batería al 50% = 12,2 V
+    Batería al 30% = 12 V
+    Batería descargada = 11,6 V
+    """
     # Get new Value
     with I2CMaster() as i2c:
         adc = PCF8591(i2c, FOUR_SINGLE_ENDED)
@@ -23,8 +33,20 @@ if __name__ == '__main__':
 
         value = ref_voltage*divider*pin.value
         print("read: {}".format(value))
+
+        if value < 11.6:
+            charge = "0%"
+        elif value < 12.0:
+            charge = "30%"
+        elif value < 12.2:
+            charge = "50%"
+        elif charge < 12.5:
+            value = "75%"
+        else:
+            charge = "100%"
+
         with open("data.txt", "a") as myfile:
-            myfile.write("{}, {}\n".format(datetime.now().strftime("%Y%m%d%H%M%S"), value))
+            myfile.write("{}, {}\n".format(date, value))
 
     # Data for plotting
     t = []
@@ -32,13 +54,13 @@ if __name__ == '__main__':
     with open('data.txt') as fp:
         for cnt, line in enumerate(fp):
             data = line.split(",")
-            t.append(int(data[0].strip()))
+            t.append(cnt)
             s.append(float(data[1].strip()))
 
     fig, ax = plt.subplots()
     ax.plot(t, s)
 
-    ax.set(xlabel='time', ylabel='voltage (V)')
+    ax.set(xlabel="{} - {}".format(date, charge), ylabel='voltage (V)')
     ax.grid()
 
     fig.savefig("battery.png")
